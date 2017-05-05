474aed68e475fc4aec9cbdc92d380c5150419e85
huyuqiong <921628437@qq.com>
2017-05-05 13:52:52 +0800

    bug

--- bin/update.js
+++ bin/update.js
@@ -1,7 +1,7 @@
 /**
  * 检查更新
  */
-function  CodePush(container, progressCon, progress, callback) {
+function CodePush(container, progressCon, progress, callback) {
     this.container = container;
     this.progressCon = progressCon;
     this.progress = progress;
@@ -9,11 +9,10 @@ function  CodePush(container, progressCon, progress, callback) {
 }
 
 CodePush.prototype = {
-    checkUpdate: function () {
-        var _this = this;
+    checkUpdate: function (container, progressCon, progress, callback) {
         var downloadProgress = function (progress) {
             var scale = Math.floor(progress.receivedBytes / progress.totalBytes * 100);
-            _this.progress.style.width = scale + '%';
+            progress.style.width = scale + '%';
         };
 
         var onPackageDownloaded = function (localPackage) {
@@ -25,35 +24,34 @@ CodePush.prototype = {
         };
 
         var onError = function (error) {
-            _this.callback && _this.callback();
-            alert('An error occurred. ' + error);
+            callback && callback();
+            console.log('An error occurred. ' + error);
         };
 
         var onInstallSuccess = function () {
-            alert('Installation succeeded.' + elProcessBar);
-            setTimeout(codePush.restartApplication, 100);
-            _this.progress.style.display = 'block';
-            _this.container.style.display = 'none';
+            console.log('Installation succeeded.' + elProcessBar);
+            setTimeout(window.codePush.restartApplication, 100);
+            progress.style.display = 'block';
+            container.style.display = 'none';
         };
 
         var onUpdateCheck = function (remotePackage) {
             if (!remotePackage) {
-                _this.callback && _this.callback();
-                _this.container.style.display = 'none';
-                alert('The application is up to date.');
+                callback && callback();
+                container.style.display = 'none';
+                console.log('The application is up to date.');
             } else {
-                _this.progressCon.style.display = 'block';
-                alert('A CodePush update is available. Package hash: ' + remotePackage.packageHash);
+                progressCon.style.display = 'block';
+                console.log('A CodePush update is available. Package hash: ' + remotePackage.packageHash);
                 remotePackage.download(onPackageDownloaded, onError, downloadProgress);
             }
         };
-
+        console.log(window.codePush);
         if (window.codePush) {
-            alert('dads');
-            _this.container.style.display = 'block';
+            container.style.display = 'block';
             window.codePush.checkForUpdate(onUpdateCheck, onError);
         } else {
-            _this.callback();
+            callback();
         }
     }
 }
