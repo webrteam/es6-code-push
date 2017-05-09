06ac0817ab97658daf2fcef5e22ea134fd99e4b4
huyuqiong <921628437@qq.com>
2017-05-09 14:52:16 +0800

    new bug

--- bin/update.js
+++ bin/update.js
@@ -12,7 +12,8 @@ CodePush.prototype = {
     checkUpdate: function (container, progressCon, progressLine, callback) {
         var downloadProgress = function (progress) {
             var scale = Math.floor(progress.receivedBytes / progress.totalBytes * 100);
-            progressLine.style.width = scale + '%';
+            if(progressLine)
+                progressLine.style.width = scale + '%';
         };
 
         var onPackageDownloaded = function (localPackage) {
@@ -25,7 +26,8 @@ CodePush.prototype = {
 
         var onError = function (error) {
             callback && callback();
-            container.style.display = 'none';
+            if(container)
+                container.style.display = 'none';
             console.log('An error occurred. ' + error);
         };
 
@@ -33,23 +35,27 @@ CodePush.prototype = {
             console.log('Installation succeeded.');
             setTimeout(window.codePush.restartApplication, 100);
             progress.style.display = 'block';
-            container.style.display = 'none';
+            if(container)
+                container.style.display = 'none';
         };
 
         var onUpdateCheck = function (remotePackage) {
             if (!remotePackage) {
                 callback && callback();
-                container.style.display = 'none';
+                if(container)
+                    container.style.display = 'none';
                 console.log('The application is up to date.');
             } else {
-                progressCon.style.display = 'block';
+                if(progressCon)
+                    progressCon.style.display = 'block';
                 console.log('A CodePush update is available. Package hash: ' + remotePackage.packageHash);
                 remotePackage.download(onPackageDownloaded, onError, downloadProgress);
             }
         };
         console.log(window.codePush);
         if (window.codePush) {
-            container.style.display = 'block';
+            if(container)
+                container.style.display = 'block';
             window.codePush.checkForUpdate(onUpdateCheck, onError);
         } else {
             callback();
